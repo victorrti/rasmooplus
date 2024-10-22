d6c957711963de51310c13341f705ec193564aa9
victorrti <victorrodriguesti@gmail.com>
2024-10-22 10:58:01 -0300

    :sparkles: feat: padronizando exceptions geradas pelo javaz validation

--- src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -5,8 +5,14 @@ import com.client.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.rasmooplus.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ResourceHandler {
@@ -20,8 +26,24 @@ public class ResourceHandler {
         String errorMessage = badRequestException.getMessage();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(montaErroResponseDto(errorMessage,HttpStatus.BAD_REQUEST,HttpStatus.BAD_REQUEST.value()));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroResponseDto>  methodArgumentNotValidException(MethodArgumentNotValidException m){
+        Map<String,String> messages = new HashMap<>();
+        m.getBindingResult().getAllErrors().forEach(error->{
+            String field = ((FieldError) error).getField();
+            String defaultMessage = error.getDefaultMessage();
+            messages.put(field,defaultMessage);
+        });
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErroResponseDto.builder()
+                .message(Arrays.toString(messages.entrySet().toArray()))
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
+    }
     public ErroResponseDto montaErroResponseDto(String message,HttpStatus httpStatus,Integer code){
         return ErroResponseDto.builder().message(message).httpStatus(httpStatus).statusCode(code).build();
     }
+
 
 }
