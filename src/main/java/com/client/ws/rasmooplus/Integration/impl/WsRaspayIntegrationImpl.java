b138f8cee411c5ff6c467c6bfc00d6780e7b9cc0
victorrti <victorrodriguesti@gmail.com>
2024-10-24 08:03:13 -0300

    implementando metodo de criar costumer na api do wsraspay

--- src/main/java/com/client/ws/rasmooplus/Integration/impl/WsRaspayIntegrationImpl.java
+++ src/main/java/com/client/ws/rasmooplus/Integration/impl/WsRaspayIntegrationImpl.java
@@ -4,13 +4,34 @@ import com.client.ws.rasmooplus.Integration.WsRaspayIntgration;
 import com.client.ws.rasmooplus.dto.wsraspay.CostumerDTO;
 import com.client.ws.rasmooplus.dto.wsraspay.OrderDTO;
 import com.client.ws.rasmooplus.dto.wsraspay.PaymentDTO;
+import org.apache.coyote.Response;
 import org.hibernate.query.Order;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+@Component
 public class WsRaspayIntegrationImpl implements WsRaspayIntgration {
+    private RestTemplate restTemplate;
+
+    private static final String URL = "http://localhost:80881/ws-raspay/";
+
+    public WsRaspayIntegrationImpl(){
+        restTemplate = new RestTemplate();
+    }
 
     @Override
     public CostumerDTO createCostumer(CostumerDTO costumerDTO) {
-        return null;
+        try{
+            HttpEntity<CostumerDTO> request = new HttpEntity<>(costumerDTO);
+            ResponseEntity<CostumerDTO> response = restTemplate.exchange(URL+"v1/custumer", HttpMethod.POST,request, CostumerDTO.class);
+            return response.getBody();
+        }catch(Exception e){
+            throw  e;
+        }
+
     }
 
     @Override
