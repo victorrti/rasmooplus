23e90633ace8dff71aa2be8ef1828ca7a460894d
victorrti <victorrodriguesti@gmail.com>
2024-10-24 16:21:51 -0300

    criando recurso para consumir o endpoint de criação de order no raspay

--- src/main/java/com/client/ws/rasmooplus/Integration/impl/WsRaspayIntegrationImpl.java
+++ src/main/java/com/client/ws/rasmooplus/Integration/impl/WsRaspayIntegrationImpl.java
@@ -7,25 +7,35 @@ import com.client.ws.rasmooplus.dto.wsraspay.PaymentDTO;
 import org.apache.coyote.Response;
 import org.hibernate.query.Order;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+
 @Component
 public class WsRaspayIntegrationImpl implements WsRaspayIntgration {
     private RestTemplate restTemplate;
+    private final HttpHeaders headers;
 
     private static final String URL = "http://localhost:80881/ws-raspay/";
 
     public WsRaspayIntegrationImpl(){
         restTemplate = new RestTemplate();
+        headers = getHttpHeaders();
     }
 
     @Override
     public CostumerDTO createCostumer(CostumerDTO costumerDTO) {
         try{
-            HttpEntity<CostumerDTO> request = new HttpEntity<>(costumerDTO);
+            String credentials= "rasmooplus:r@asm00";
+            String base64 = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
+            this.headers.add("Authorization","Basic "+base64);
+            HttpEntity<CostumerDTO> request = new HttpEntity<>(costumerDTO,this.headers);
             ResponseEntity<CostumerDTO> response = restTemplate.exchange(URL+"v1/custumer", HttpMethod.POST,request, CostumerDTO.class);
             return response.getBody();
         }catch(Exception e){
@@ -36,11 +46,28 @@ public class WsRaspayIntegrationImpl implements WsRaspayIntgration {
 
     @Override
     public OrderDTO createOrder(OrderDTO orderDTO) {
-        return null;
+        try{
+            String credentials= "rasmooplus:r@asm00";
+            String base64 = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
+            this.headers.add("Authorization","Basic "+base64);
+            HttpEntity<OrderDTO> request = new HttpEntity<>(orderDTO,this.headers);
+            ResponseEntity<OrderDTO> response = restTemplate.exchange(URL+"v1/order", HttpMethod.POST,request, OrderDTO.class);
+            return response.getBody();
+        }catch(Exception e){
+            throw  e;
+        }
     }
 
     @Override
     public Boolean processPayment(PaymentDTO dto) {
         return null;
+    }
+
+    private static HttpHeaders getHttpHeaders() {
+        String credentials= "rasmooplus:r@asm00";
+        String base64 = Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization","Basic "+base64);
+        return headers;
     }
 }
--- src/test/java/com/client/ws/rasmooplus/Integration/WsRaspayIntegrationImplTest.java
+++ src/test/java/com/client/ws/rasmooplus/Integration/WsRaspayIntegrationImplTest.java
@@ -1,6 +1,8 @@
 package com.client.ws.rasmooplus.Integration;
 
 import com.client.ws.rasmooplus.dto.wsraspay.CostumerDTO;
+import com.client.ws.rasmooplus.dto.wsraspay.OrderDTO;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +15,12 @@ import org.springframework.boot.test.context.SpringBootTest;
     void createCustumerWhenDtoOk(){
          CostumerDTO dto = new CostumerDTO(null,"22994287048","teste@teste.com","victor","pereira");
          wsRaspayIntgration.createCostumer(dto);
+
+    }
+    @Test
+    void creteOrderWhenDtoOk(){
+        OrderDTO dto = new OrderDTO(null,"22994287048",10L,"ProductMensal");
+        wsRaspayIntgration.createOrder(dto);
 
     }
 }
