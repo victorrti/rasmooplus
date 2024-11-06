93f5aeb9729cacb3e1725f80370a2db24a690e93
victorrti <victorrodriguesti@gmail.com>
2024-11-06 09:32:02 -0300

    recriando teste sem contexto spring com Mock e injectMocks

--- src/test/java/com/client/ws/rasmooplus/service/UserTypeServiceTest.java
+++ src/test/java/com/client/ws/rasmooplus/service/UserTypeServiceTest.java
@@ -1,10 +1,15 @@
 package com.client.ws.rasmooplus.service;
 
 import com.client.ws.rasmooplus.Model.jpa.UserType;
+import com.client.ws.rasmooplus.repository.jpa.UserTypeRepository;
+import com.client.ws.rasmooplus.service.impl.UserTypeServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,12 +17,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
  class UserTypeServiceTest {
-    @MockBean
-    UserTypeService userTypeService;
+    @Mock
+    private UserTypeRepository userTypeRepository;
+    @InjectMocks
+    private UserTypeServiceImpl userTypeService;
     @Test
-    void findAll(){
+    void given_findAll_when_thereAreDatasInDataBase_then_returnAllDatas(){
        List<UserType> listUserType = new ArrayList<>();
        UserType userType1 = new UserType(1L,"ADMIN","Usuario  administrador");
        UserType userType2 = new UserType(1L,"PROFESSOR","Usuario  professor");
@@ -25,8 +32,9 @@ import java.util.List;
        listUserType.add(userType1);
        listUserType.add(userType2);
        listUserType.add(userType3);
-       Mockito.when(userTypeService.findAll()).thenReturn(listUserType);
+       Mockito.when(userTypeRepository.findAll()).thenReturn(listUserType);
        Assertions.assertThat(userTypeService.findAll()).isNotEmpty();
+       Assertions.assertThat(userTypeService.findAll()).hasSize(3);
 
     }
 }
