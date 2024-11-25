d688a7906beda19e9959ea0d9ab727f270a45112
victorrti <victorrodriguesti@gmail.com>
2024-11-25 14:16:14 -0300

    criando builder do auth dto do keycloak

--- src/main/java/com/client/ws/rasmooplus/dto/KeycloakOauthDto.java
+++ src/main/java/com/client/ws/rasmooplus/dto/KeycloakOauthDto.java
@@ -4,10 +4,51 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class KeycloakOauthDto {
+    private final MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+    public static KeycloakOauthDto builder(){
+        return new KeycloakOauthDto();
+    }
+
+    public KeycloakOauthDto clientId(String clientId){
+        params.add("client_id",clientId);
+        return this;
+    }
+    public KeycloakOauthDto username(String username){
+        params.add("username",username);
+        return this;
+    }
+    public KeycloakOauthDto password(String paswword){
+        params.add("password",paswword);
+        return this;
+    }
+    public KeycloakOauthDto clientSecret(String clientSecret){
+        params.add("client_secret",clientSecret);
+        return this;
+    }
+    public KeycloakOauthDto grantType(String grantType){
+        params.add("grant_type",grantType);
+        return this;
+    }
+    public KeycloakOauthDto refreshToken(String refreshToken){
+        params.add("refresh_token",refreshToken);
+        return this;
+    }
+
+    public MultiValueMap<String,String> build(){
+        return params;
+    }
+
 }
